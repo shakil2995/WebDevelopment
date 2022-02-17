b1fe873c4c3ad562dea728d94deb0ff6cca9a0ca
Shakil <shakil2995@gmail.com>
2022-02-18 01:02:05 +0600

    working with server

--- expressApps/calculator/calculator.js
+++ expressApps/calculator/calculator.js
@@ -3,17 +3,47 @@ const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+
+
 app.get('/', function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
+app.get('/additioncalculator', function(req,res){
+    res.sendFile(__dirname+"/additioncalculator.html");
+})
 
-app.post('/', function(req,res){
+app.get('/bmicalculator', function(req,res){
+    res.sendFile(__dirname+"/bmiCalculator.html");
+})
+
+
+
+
+
+
+app.post('/additioncalculator', function(req,res){
     let num1 =Number(req.body.num1);
     let num2= Number(req.body.num2);
     let add = num1+num2;
     res.send(`${add}`);
+})
+
+app.post('/bmicalculator', function(req,res){
+    // res.send("Working");
+    let weight = req.body.weight;
+    let height = req.body.height;
+    bmi = weight - (height*5);
+    res.send(`Your BMI is ${bmi}`);
     // console.log(req.body);
 })
+
+
+
+
+
+
+
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
