d64908381e91f5c1f33c73a6357d25160b01aca8
Shakil <shakil2995@gmail.com>
2022-02-17 20:49:20 +0600

    first app with express

--- expressServer/server.js
+++ expressServer/server.js
@@ -5,8 +5,16 @@ const app = express();
 
 app.get("/",function(req, res){
     res.send("<h1>Hello world</h1>");
-})
-
+});
+app.get("/images",function(req, res){
+    res.send('<img src="shakil.jpg" alt="shakil image">');
+});
+app.get("/about",function(req, res){
+    res.send("<h1>I am Shakil</h1>");
+});
+app.get("/contact",function(req, res){
+    res.send("<h1>Phone : 01551525</h1>");
+});
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
