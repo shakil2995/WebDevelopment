5d3fcefa1eff975c78c043d029f5f750481a3786
Shakil <shakil2995@gmail.com>
2022-02-17 21:56:46 +0600

    calculator with backend functionality

--- expressApps/calculator/calculator.js
+++ expressApps/calculator/calculator.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req,res){
-    res.send("App is running");
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post('/', function(req,res){
+    let num1 =Number(req.body.num1);
+    let num2= Number(req.body.num2);
+    let add = num1+num2;
+    res.send(`${add}`);
+    // console.log(req.body);
 })
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
+    // console.log(__dirname);
 });
