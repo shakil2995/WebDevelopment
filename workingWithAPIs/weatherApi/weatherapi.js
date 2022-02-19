1ef2c146d487b1e79c9626f4c1262ce3895a93ff
Shakil <shakil2995@gmail.com>
2022-02-19 20:33:51 +0600

    Update weatherapi.js

--- workingWithAPIs/weatherApi/weatherapi.js
+++ workingWithAPIs/weatherApi/weatherapi.js
@@ -2,18 +2,22 @@ const express = require('express');
 let app = express();
 const https = require('https');
 
-const apiLink="https://api.openweathermap.org/data/2.5/weather?q=Dhaka&units=metric&appid=398e310b24f291b753fabdb60b31cc14"
+const url="https://api.openweathermap.org/data/2.5/weather?q=Dhaka&units=metric&appid=398e310b24f291b753fabdb60b31cc14"
 
 
 app.get('/', function(req,res) {
-    res.send("Hello");
-    
-    https.get(apiLink,function(response){
-        console.log('statusCode : ',response.statusCode);
-        console.log('headers :',response.headers);
+    https.get(url,function(response){
+        console.log(response.statusCode);
+        
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+
+            res.send(`It's ${weatherData.weather[0].main}y outside`)
+            console.log('statusCode : ',weatherData.weather[0].main);
         })
+    })
 })
 
 app.listen(3000, function(req,res){
-    console.log("Working");
+    //console.log("Working");
 })
