cd71ccdc7e012e81e38b1bc458b94fbc1e2809b3
Shakil <shakil2995@gmail.com>
2022-02-22 01:57:44 +0600

    Update app.js

--- workingWithAPIs/newsletterApp/app.js
+++ workingWithAPIs/newsletterApp/app.js
@@ -10,16 +10,44 @@ app.get('/', function(req,res) {
 })
 
 app.post('/', function(req,res) {
-    let fName = req.body.fName;
-    let lName = req.body.lName;
-    let email = req.body.email;
-        res.send("Welcome, <b>" + fName +" "+ lName + "</b><br> Your email address is <b>"+email+"</b>");
-    // res.sendFile(__dirname+"/static/success.html");
- })
+    const fName = req.body.fName;
+    const lName = req.body.lName;
+    const email = req.body.email;
+    const url = 'https://us14.api.mailchimp.com/3.0/lists/25b7e0694e';
+    const options = {
+        method : "POST",
+        auth : 'https://<dc>.api.mailchimp.com/3.0/',
+    }
+const data = {
+    members : [
+        {
+        email_address:email,
+        status : "Subscribed",
+        merge_fields : {
+            FNAME : fName,
+            LNAME : lName
+            }
+        }
+    ]
+}
+const jsonData = JSON.stringify(data);
+
+const request = https.request(url,options,function(response){
+    response.on("data ", function(data){
+        console.log(JSON.parse(data));
+    })
+    request.write(jsonData);
+    request.end();
+});
+
+//         res.send("Welcome, <b>" + fName +" "+ lName + "</b><br> Your email address is <b>"+email+"</b>");
+// res.sendFile(__dirname+"/static/success.html");
+// apiurl  'https://<dc>.api.mailchimp.com/3.0/'
+//apikey 7d71257ab545258714652902b2d95f36-us14  
+// auid 25b7e0694e 
+})
+
 app.listen(3000,function(req,res){
     console.log("server Working");
 })
 
-
-// apikey
-// 7d71257ab545258714652902b2d95f36-us14  
