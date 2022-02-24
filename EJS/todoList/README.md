0d2d6ffddfd776e2ab342da5b8b6cf80b82ae230
Shakil <shakil2995@gmail.com>
2022-02-24 19:17:38 +0600

    started ejs

---
+++ EJS/todoList/README.md
@@ -0,0 +1,1 @@
+# Fullstack To-Do list app
---
+++ EJS/todoList/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const https = require('https');
+
+app.get('/', function (req, res){
+    // res.sendFile(__dirname+"/static/index.html");
+    let date = new Date();
+    res.send(JSON.parse(date));
+})
+
+app.post('/', function (req, res){
+    res.sendFile(__dirname+"/static/success.html");
+})
+
+
+// SERVER 
+app.listen(3000,function(req,res){
+    console.log("server working on port 3000");
+}) 
