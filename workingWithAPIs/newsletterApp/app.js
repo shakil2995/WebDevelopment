13582f227f883751692e35bb0eba19f0a709bd85
Shakil <shakil2995@gmail.com>
2022-02-24 17:36:23 +0600

    done

--- workingWithAPIs/newsletterApp/app.js
+++ workingWithAPIs/newsletterApp/app.js
@@ -15,8 +15,7 @@ app.post('/', function(req,res) {
     const lName = req.body.lName;
     const email = req.body.email;
     const url = "https://us14.api.mailchimp.com/3.0/lists/25b7e0694e";
-    console.log(req.body);
-   
+    // console.log(req.body); 
 // //  Data class
 const data = {
     members : [
@@ -30,8 +29,7 @@ const data = {
         }
     ]
 };
-
-const dataPost = JSON.stringify(data);
+const jsonData = JSON.stringify(data);
 
 const options = {
     url:'https://us14.api.mailchimp.com/3.0/lists/25b7e0694e',
@@ -39,47 +37,28 @@ const options = {
     headers:{
         Authorization : 'auth 7d71257ab545258714652902b2d95f36-us14'
     },
-    body : dataPost
-
+    body : jsonData
 } 
-
-if(email){
-    request(options,(err,response,body)=>{
-        if(err){
-            res.json({error:err})
-        } else{
-            res.redirect(__dirname+'/static/success.html')
-        }
-    })
-    console.log("Got Email");
-} else {
-    res.status(404).send({message : 'Failed'});
-}
-
-//  london app
-// const jsonData = JSON.stringify(data);
-// // created options 
-// const options = {
-//     method : "POST",
-//     auth : "shakil:7d71257ab545258714652902b2d95f36-us14"
-//     // auth : 'https://us14.api.mailchimp.com/3.0/',
-// } 
-
-
-// const request = https.request(url,options,function(response){
-//     response.on("data", function(data){
-//         console.log(JSON.parse(data));
-//     })
-//     request.write(jsonData);
-//     request.end(); 
-// });
-// res.send("Welcome, <b>" + fName +" "+ lName + "</b><br> Your email address is <b>"+email+"</b>");
-// res.sendFile(__dirname+"/static/success.html");
-// apiurl  'https://<dc>.api.mailchimp.com/3.0/'
-//apikey 7d71257ab545258714652902b2d95f36-us14  
-// auid 25b7e0694e 
+const request = https.request(url,options,function(response){
+    response.on("data",function(data){
+        // console.log(JSON.parse(data));
+        // console.log(response.statusCode);
+        if(response.statusCode===200){
+            res.sendFile(__dirname+"/static/success.html");}
+            else {
+                res.sendFile(__dirname+"/static/failure.html");
+            }
+    });
+});
+request.write(jsonData);
+request.end();
 })
-app.listen(3000,function(req,res){
+
+app.post("/failure",function(req,res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function(req,res){
     console.log("server Working");
 })
 
